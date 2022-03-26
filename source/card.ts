c4ef69032bcef81a0ce00e0ccff6449c04e3e1a4
ET <blueprin4@gmail.com>
2022-03-26 18:41:06 -0500

    new2

---
+++ source/card.ts
@@ -0,0 +1,17 @@
+
+class Card {
+
+    value:number;
+    color:string;
+    isSpecial: boolean;
+    isspecial: boolean;
+    constructor(value: number, color: string, isSpecial: boolean) {
+        this.value = value;
+        this.color = color;
+        this.isSpecial = isSpecial;
+    }
+    
+
+}
+
+export default Card;
